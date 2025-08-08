f13e1fce3cf7df789c3fcf1fcc77da5570a4e565
Misutka364 <denmisa929@gmail.com>
2025-08-08 19:17:36 +0300

    Update nscript.js

--- nscript.js
+++ nscript.js
@@ -1,48 +1,47 @@
-let numberInput = document.getElementById('number');
-let mayanInput = document.getElementById('mayan');
-let literalInput = document.getElementById('in-two-words');
+function zeroToTwenty(num) {
+  if(num === 0) return [0];
+  let digits = [];
+  while(num > 0) {
+    digits.unshift(num % 20);
+    num = Math.floor(num / 20);
+  }
+  return digits;
+}
 
-numberInput.addEventListener('input', function() {
-  mayanInput.value = '';
-  literalInput.value = '';
+function digitToMayan(digit) {
+  if(digit === 0) return "⨀"; 
+
+  const fiveCount = Math.floor(digit / 5);
+  const oneCount = digit % 5;
+
   
-  let numberValue = parseInt(numberInput.value, 10);
-  
-  let twenties = Math.floor(numberValue / 20);
-  let remainder = numberValue % 20;
-  
-  let mayan = '';
-  
-  for (let i = 0; i < twenties; i++) {
-    mayan += '●';
+  const minus = "−"; 
+  const dot = "●";
+
+  return minus.repeat(fiveCount) + dot.repeat(oneCount);
+}
+
+function changeNum() {
+  let value = parseInt(document.getElementById("number").value, 10);
+  if (isNaN(value) || value < 0) {
+    value = 0;
   }
-  
-  if (twenties > 0) {
-    mayan += '.';
-  }
-  
-  if (remainder === 0) {
-    mayan += '⨀';
-  } else if (remainder <= 4) {
-    for (let i = 0; i < remainder; i++) {
-      mayan += '●';
-    }
-  } else if (remainder % 5 === 0) {
-    let lines = remainder / 5;
-    for (let i = 0; i < lines; i++) {
-      mayan += '−';
-    }
-  } else {
-    let lines = Math.floor(remainder / 5);
-    let dots = remainder % 5;
-    for (let i = 0; i < lines; i++) {
-      mayan += '−';
-    }
-    for (let i = 0; i < dots; i++) {
-      mayan += '●';
-    }
-  }
-  
-  mayanInput.value = mayan;
-  literalInput.value = numberValue;
-});
+
+  const digits = zeroToTwenty(value);
+
+  const mayan = digits.map(digitToMayan).join(".");
+
+  document.getElementById("mayan").value = mayan;
+
+  const explain = digits
+    .map(function(n, i) {
+      let expr;
+      if (n === 0) expr = "0";
+      else if (n < 5) expr = n.toString();
+      else expr = "(" + Math.floor(n / 5) + " x 5 + " + (n % 5) + ")";
+      const power = digits.length - i - 1;
+      return expr + " x 20^" + power;
+    }).join(" + \n") + " = " + value;
+
+  document.getElementById("in-two-words").value = explain;
+}
